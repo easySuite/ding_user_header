09d935b898a455afa6a61d5884586195e4ce1404
Alex Damaschin <ad@peytz.dk>
2016-06-03 23:48:18 +0300

    EASYOPAC-95 - Adding 'error' class to inputs in login form.

--- js/ding_user_header.js
+++ js/ding_user_header.js
@@ -3,15 +3,26 @@
 
   $(document).ready(function() {
     // Moves the search form into the search result page.
-    $('.ding-user-header-block').parents('.site-header').addClass('new-header').removeClass('site-header');
+    $('.js-topbar-search').parents('.site-header').addClass('new-header').removeClass('site-header');
 
     var search = $('.primary-content .search .search-field-wrapper');
     search.find('.form-item-search-block-form label').remove();
     search.addClass('search-field-in-content');
     search.addClass('js-search-field-in-content');
 
+    // Weird way of adding 'error' class to inputs in login form.
+    var hiddenLoginFormName = $('.js-topbar-user .form-item-name input');
+    var hiddenLoginFormPass = $('.js-topbar-user .form-item-pass input');
 
-    $('.js-topbar-user').remove();
+    var displayedLoginFormName = $('.ding-user-header-block .form-item-name input');
+    var displayedLoginFormPass = $('.ding-user-header-block .form-item-pass input');
 
+    if (hiddenLoginFormName.hasClass('error')) {
+      displayedLoginFormName.addClass('error');
+    }
+
+    if (hiddenLoginFormPass.hasClass('error')) {
+      displayedLoginFormPass.addClass('error');
+    }
   });
 }(jQuery));
